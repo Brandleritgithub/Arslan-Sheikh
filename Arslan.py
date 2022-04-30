932d852ae9059a09caa784802267a2affe6f729b
Brandleritgithub <104646468+Brandleritgithub@users.noreply.github.com>
2022-04-30 14:54:32 +0500

    Update Arslan.py

--- Arslan.py
+++ Arslan.py
@@ -1,6 +1,6 @@
 print ("       Thanks To Using My Tool")
 
-print ("\033[1:33m         Admin :- Arslan x Ahmad")
+print ("\033[1;33m         Admin :- Arslan x Ahmad")
 
 print ("\033[1;32m      Enjoy ")
 
